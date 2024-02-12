d5d95dc9409513c3f2beb7b1fe91e8c749ed7b0c
fatihkurtl <kurtf061@gmail.com>
2024-02-13 01:27:25 +0300

    product table edited

--- frontend/src/pages/admin/Products.vue
+++ frontend/src/pages/admin/Products.vue
@@ -5,7 +5,7 @@ import MainLayout from '@/layouts/admin/MainLayout.vue'
 
 <template>
     <MainLayout>
-        <section class="bg-gray-50 dark:bg-gray-900 py-3 sm:py-5">
+        <section class="bg-mainBgColor dark:bg-gray-900 rounded-lg">
             <div class="min-w-full bg-mainBgColor">
                 <div class="bg-white rounded-lg dark:bg-gray-800 md:pl-70 mx-4 shadow-md sm:rounded-lg overflow-hidden">
                     <div
@@ -23,13 +23,16 @@ import MainLayout from '@/layouts/admin/MainLayout.vue'
                         <div
                             class="flex flex-col flex-shrink-0 space-y-3 md:flex-row md:items-center lg:justify-end md:space-y-0 md:space-x-3">
                             <button type="button"
-                                class="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
-                                <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20"
+                                class="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                                <!-- <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20"
                                     xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                     <path clip-rule="evenodd" fill-rule="evenodd"
                                         d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
-                                </svg>
-                                Add new product
+                                </svg> -->
+                                <span class="material-symbols-outlined h-6 w-6 mr-2">
+                                    add
+                                </span>
+                                Yeni ürün ekle
                             </button>
                             <button type="button"
                                 class="flex items-center justify-center flex-shrink-0 px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
