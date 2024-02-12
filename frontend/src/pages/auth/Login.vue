068efff1a2d82410e0de6538c66639fd3f26b662
fatihkurtl <kurtf061@gmail.com>
2024-02-13 01:22:35 +0300

    product table added

--- frontend/src/pages/auth/Login.vue
+++ frontend/src/pages/auth/Login.vue
@@ -2,7 +2,7 @@
 import { ref, reactive } from 'vue'
 import awsIcon from '@/assets/aws_icon.png'
 import Layout from '@/layouts/base/Layout.vue'
-import type { LoginData } from '@/interfaces/login' 
+import type { LoginData } from '@/interfaces/login'
 
 const appName = ref<string>(import.meta.env.VITE_APP_NAME)
 
@@ -10,8 +10,12 @@ const appName = ref<string>(import.meta.env.VITE_APP_NAME)
 const loginData = reactive<LoginData>({
   email: null,
   password: null,
-  rememberMe: null
+  rememberMe: false
 })
+
+const signIn = (): void => {
+  console.log(loginData)
+}
 
 
 </script>
@@ -29,27 +33,26 @@ const loginData = reactive<LoginData>({
           <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
             Hesabınızda oturum açın
           </h1>
-          <form class="space-y-4 md:space-y-6" action="#">
+          <form @submit.prevent="signIn" class="space-y-4 md:space-y-6">
             <div>
               <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                 E-posta adresiniz
               </label>
-              <input type="email" name="email" id="email"
+              <input v-model="loginData.email" type="email" name="email" id="email"
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="name@company.com" required>
             </div>
             <div>
               <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Şifre</label>
-              <input type="password" name="password" id="password" placeholder="••••••••"
+              <input v-model="loginData.password" type="password" name="password" id="password" placeholder="••••••••"
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 required>
             </div>
             <div class="flex items-center justify-between">
               <div class="flex items-start">
                 <div class="flex items-center h-5">
-                  <input id="remember" aria-describedby="remember" type="checkbox"
-                    class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                    required>
+                  <input v-model="loginData.rememberMe" id="remember" aria-describedby="remember" type="checkbox"
+                    class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800">
                 </div>
                 <div class="ml-3 text-sm">
                   <label for="remember" class="text-gray-500 dark:text-gray-300">Beni hatırla</label>
