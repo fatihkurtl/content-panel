f7a43c999e1a4f4f4d640f6c1fe3b27030c8e78e
fatihkurtl <kurtf061@gmail.com>
2024-02-28 18:14:11 +0300

    home page edited

--- frontend/src/pages/admin/Home.vue
+++ frontend/src/pages/admin/Home.vue
@@ -68,6 +68,24 @@ console.log('.env variable =>', import.meta.env.VITE_APP_NAME)
                            <!-- Total Sales -->
                         </div>
                         <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64">
+                           <!-- Customers -->
+                           <div
+                              class="max-w-sm min-h-full bg-white border border-gray-200 rounded-lg shadow p-6 hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 flex justify-between items-center">
+                              <div>
+                                 <p class="text-2xl font-bold text-gray-900 items-center">
+                                    258
+                                 </p>
+                                 <p class="text-gray-700">Toplam müşteri</p>
+                              </div>
+                              <div>
+                                 <svg class="w-12 h-12 text-green-500" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+                                       d="M4.5 17H4a1 1 0 0 1-1-1 3 3 0 0 1 3-3h1m0-3a2.5 2.5 0 1 1 2-4.5M19.5 17h.5c.6 0 1-.4 1-1a3 3 0 0 0-3-3h-1m0-3a2.5 2.5 0 1 0-2-4.5m.5 13.5h-7a1 1 0 0 1-1-1 3 3 0 0 1 3-3h3a3 3 0 0 1 3 3c0 .6-.4 1-1 1Zm-1-9.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z" />
+                                 </svg>
+                              </div>
+                           </div>
+                           <!-- Customers -->
                         </div>
                         <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64">
                         </div>
