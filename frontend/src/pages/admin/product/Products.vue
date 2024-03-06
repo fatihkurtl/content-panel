c49cd3d82f1cfa09796a14a9cc77a243f7f2c6dc
fatihkurtl <kurtf061@gmail.com>
2024-03-06 17:51:00 +0300

    product stock edited

--- frontend/src/pages/admin/product/Products.vue
+++ frontend/src/pages/admin/product/Products.vue
@@ -13,11 +13,13 @@ const apiService = new ApiService()
 const productData = ref()
 const filledStars = computed(() => Math.round(productData.value.rating))
 
-apiService.get().then(response => {
-    console.log(response.data.products)
-    productData.value = response.data.products
-}).catch(error => {
-    console.error('Error:', error)
+onMounted(async () => {
+    apiService.get().then(response => {
+        console.log(response.data.products)
+        productData.value = response.data.products
+    }).catch(error => {
+        console.error('Error:', error)
+    })
 })
 
 const {
@@ -436,8 +438,8 @@ const {
                                     </td>
                                     <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                         <div class="flex items-center">
-                                            <div :class="{ 'bg-green-700': product.stock > 100 }"
-                                                class="inline-block w-4 h-4 mr-2 bg-red-700 rounded-full"></div>
+                                            <div :class="{ 'bg-green-700': product.stock >= 100, 'bg-red-700': product.stock < 100 }"
+                                                class="inline-block w-4 h-4 mr-2 rounded-full"></div>
                                             {{ product.stock }}
                                         </div>
                                     </td>
@@ -450,7 +452,7 @@ const {
                                     <td class="px-4 py-2 font-medium whitespace-nowrap dark:text-white">
                                         <div class="flex items-center">
                                             <svg v-for="star in 5" aria-hidden="true"
-                                            :class="{ 'text-yellow-400': star <= product.rating, 'text-gray-300': star > filledStars }"
+                                                :class="{ 'text-yellow-400': star <= product.rating, 'text-gray-300': star > filledStars }"
                                                 class="w-5 h-5" fill="currentColor" viewbox="0 0 20 20"
                                                 xmlns="http://www.w3.org/2000/svg">
                                                 <path
@@ -477,7 +479,7 @@ const {
                                                     d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                                             </svg> -->
                                             <span class="ml-1 text-gray-500 dark:text-gray-400">
-                                            {{ product.rating }}</span>
+                                                {{ product.rating }}</span>
                                         </div>
                                     </td>
                                     <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
